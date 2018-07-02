15c14db8eb6de9defa6485d7e289470d739a4fac
fanpengfei <fanpengfei@jd.com>
2018-07-02 07:17:06 -0400

    update for http handler

--- main.go
+++ main.go
@@ -9,9 +9,6 @@ import (
 	"github.com/spf13/cobra"          /* CLI lib */
 	"github.com/spf13/viper"          /* Configuration lib */
 	"github.com/valyala/fasthttp"     /* http parse lib */
-	"io"
-	"net"
-	"net/http"
 	"os"
 	"strconv"
 	"strings"
@@ -26,14 +23,17 @@ type scratch struct {
 }
 
 var (
-	Version  string
-	Debug    bool
-	Port     int
+	Version string
+	Debug   bool
+	Port    int
+	Flag    string
+	Uptime  time.Time
+
+	/* TODO: 以下元素需要封装成对象，每个参数一个对象 */
+	/* TODO: 目前只能读一个文件 ? */
 	FilePath string
-	Flag     string
 	Scratch  scratch
 	Db       hyperscan.BlockDatabase
-	Uptime   time.Time
 	RegexMap map[int]RegexLine
 )
 
@@ -63,7 +63,7 @@ func main() {
 	Version = "0.0.1"
 	viper.AutomaticEnv()
 	var rootCmd = &cobra.Command{
-		Use:     "gohs-ladon",
+		Use:     "hwaf",
 		Short:   fmt.Sprintf("Gohs-ladon Service %s", Version),
 		Run:     run,
 		PreRunE: preRunE,
@@ -75,7 +75,7 @@ func main() {
 
 	viper.BindPFlag("debug", rootCmd.Flags().Lookup("debug"))
 	viper.BindPFlag("port", rootCmd.Flags().Lookup("port"))
-	viper.BindPFlag("filepath", rootCmd.Flags().Lookup("filepath"))
+	viper.BindPFlag("filepath", rootCmd.Flags().Lookup("filepath")) /* every arg is a file */
 	viper.BindPFlag("flag", rootCmd.Flags().Lookup("flag"))
 
 	rootCmd.Execute()
@@ -85,7 +85,7 @@ func run(cmd *cobra.Command, args []string) {
 	addr := fmt.Sprintf("0.0.0.0:%d", Port)
 
 	Uptime = time.Now()
-	fmt.Printf("[%s] gohs-ladon %s Running on %s\n", Uptime.Format(time.RFC3339), Version, addr)
+	fmt.Printf("[%s] hwaf %s Running on %s\n", Uptime.Format(time.RFC3339), Version, addr)
 
 	h := requestHandler
 	if err := fasthttp.ListenAndServe(addr, h); err != nil {
@@ -108,8 +108,13 @@ func preRunE(cmd *cobra.Command, args []string) error {
 		log.SetLevel(log.InfoLevel)
 	}
 	log.Debug("Prerun", args)
+
+	/* used for store rules */
 	RegexMap = make(map[int]RegexLine)
+
+	/* TODO: 需要编译多个包含scratch的处理对象 */
 	err := buildScratch(FilePath)
+
 	return err
 }
 
@@ -189,70 +194,65 @@ func buildScratch(filepath string) (err error) {
 	return nil
 }
 
-func middleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		next.ServeHTTP(w, r)
-		end := time.Now()
-		latency := end.Sub(start)
-		host, _, _ := net.SplitHostPort(r.RemoteAddr)
-		log.WithFields(log.Fields{
-			"remote_addr":    host,
-			"latency":        latency,
-			"content_length": r.ContentLength,
-		}).Info(fmt.Sprintf("%s %s", r.Method, r.RequestURI))
-	})
-}
-
-func matchHandle(w http.ResponseWriter, r *http.Request) {
-	query := r.FormValue("q")
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	//func matchHandle(w http.ResponseWriter, r *http.Request) {
 	var resp Response = Response{Errno: 0}
-	w.Header().Set("Content-Type", "application/json")
-	if query == "" {
-		resp.Errno = -1
-		resp.Msg = "empty param q"
-	} else {
-		inputData := []byte(query)
-		// results
-		var matchResps []MatchResp
-		eventHandler := func(id uint, from, to uint64, flags uint, context interface{}) error {
-			log.Info(fmt.Sprintf("id: %d, from: %d, to: %d, flags: %v, context: %s", id, from, to, flags, context))
-			regexLine, ok := RegexMap[int(id)]
-			if !ok {
-				regexLine = RegexLine{}
-			}
-			matchResp := MatchResp{Id: int(id), From: int(from), To: int(to), Flags: int(flags), Context: fmt.Sprintf("%s", context), RegexLinev: regexLine}
-			matchResps = append(matchResps, matchResp)
-			return nil
-		}
+	ctx.Response.Header.Set("Content-Type", "application/json")
 
-		// lock scratch
-		Scratch.Lock()
-		if err := Db.Scan(inputData, Scratch.s, eventHandler, inputData); err != nil {
-			logFields := log.Fields{"query": query}
-			log.WithFields(logFields).Error(err)
-			resp.Errno = -2
-			resp.Msg = fmt.Sprintf("Db.Scan error: %s", err)
-		} else {
-			if len(matchResps) <= 0 {
-				resp.Errno = 1
-				resp.Msg = "no match"
-			}
-			resp.Data = matchResps
+	inputData := []byte(ctx.RequestURI())
+
+	/*
+		fmt.Fprintf(ctx, "Hello, world!\n\n")
+		fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
+		fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
+		fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
+		fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
+		fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
+		fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
+		fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
+		fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
+		fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
+		fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
+		fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
+	*/
+
+	// results
+	var matchResps []MatchResp
+	eventHandler := func(id uint, from, to uint64, flags uint, context interface{}) error {
+		log.Info(fmt.Sprintf("id: %d, from: %d, to: %d, flags: %v, context: %s", id, from, to, flags, context))
+		regexLine, ok := RegexMap[int(id)]
+		if !ok {
+			regexLine = RegexLine{}
 		}
-		// unlock scratch
-		Scratch.Unlock()
+		matchResp := MatchResp{Id: int(id), From: int(from), To: int(to), Flags: int(flags), Context: fmt.Sprintf("%s", context), RegexLinev: regexLine}
+		matchResps = append(matchResps, matchResp)
+		return nil
 	}
-	json.NewEncoder(w).Encode(resp)
-	w.WriteHeader(http.StatusOK)
+
+	// lock scratch
+	Scratch.Lock()
+	if err := Db.Scan(inputData, Scratch.s, eventHandler, inputData); err != nil {
+		/* TODO  */
+		logFields := log.Fields{"RequestURI": ctx.RequestURI()}
+
+		log.WithFields(logFields).Error(err)
+		resp.Errno = -2
+		resp.Msg = fmt.Sprintf("Db.Scan error: %s", err)
+	} else {
+		if len(matchResps) <= 0 {
+			resp.Errno = 1
+			resp.Msg = "no match"
+		}
+		resp.Data = matchResps
+	}
+	// unlock scratch
+	Scratch.Unlock()
+
+	json.NewEncoder(ctx.Response.BodyWriter()).Encode(resp)
+	ctx.Response.Header.SetStatusCode(fasthttp.StatusForbidden)
 }
 
-func statsHandle(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, fmt.Sprintf("gohs-ladon %v, Uptime %v",
-		Version, Uptime.Format(time.RFC3339)))
-}
-
+/*
 func requestHandler(ctx *fasthttp.RequestCtx) {
 	fmt.Fprintf(ctx, "Hello, world!\n\n")
 
@@ -280,3 +280,4 @@ func requestHandler(ctx *fasthttp.RequestCtx) {
 	c.SetValue("cookie-value")
 	ctx.Response.Header.SetCookie(&c)
 }
+*/
